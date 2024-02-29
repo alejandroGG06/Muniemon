349bf78fa472ae6fcb5d9fa946dcc8a1849e2932
alejandro <grajalesalejandro068@gmail.com>
2024-02-29 13:58:50 +0100

    actualizacion muniemon

--- Actividad18/src/pokemon/main_poke.java
+++ Actividad18/src/pokemon/main_poke.java
@@ -7,15 +7,27 @@ public class main_poke {
 		
 		Scanner muni = new Scanner(System.in);
         muniemon Mm = new muniemon();
-        
+        muniemon Mm2= new muniemon();
         
         int opcion;
         do {
         	System.out.println("bienvenido al menu de creacion");
+        	System.out.println();
         	System.out.println("pulse 1 para crear el munimon ");
-        	System.out.println("pulse 2 para mostrar el munimon");
-        	System.out.println("pulse 3 para salir del programa");
-        opcion = muni.nextInt();
+        	System.out.println();
+        	System.out.println("pulse 2 para crear el segundo munimon");
+        	System.out.println();
+        	System.out.println("pulse 3 para mostrar el primer muniemon");
+        	System.out.println();
+        	System.out.println("pulse 4 para mostrar el segundo muniemon");
+        	System.out.println();
+        	System.out.println("pulse 5 para atacar al segundo muneimon");
+        	System.out.println();
+        	System.out.println("pulse 6 para atacar al segundo muneimon");
+        	System.out.println();
+        	System.out.println("pulse 7 para salir");
+        
+        	opcion = muni.nextInt();
 		
         	
        
@@ -44,24 +56,66 @@ public class main_poke {
 		break;
 
 	case 2:
-		 System.out.println("Datos del Muniemon:");
-         System.out.println("Nombre: " + Mm.nombre ); 
-         System.out.println("Vida: "  + Mm.vida);
-         System.out.println("Ataque: " + Mm.ataque) ;
-         System.out.println("Defensa: " + Mm.defensa); 
-         System.out.println("Tipo: " + Mm.tipo );
+		System.out.println("introduzca el nombre del muniemon");
+		 nombre=muni.next();	
+		
+		System.out.println("introduzca la vida del munimon");
+		 vida= muni.nextInt();
+		
+		System.out.println("introduzca el ataque");
+		 ataque=muni.nextInt();
+		
+		System.out.println("introduzca los puntos de defensa");
+		 defensa=muni.nextInt();
+		
+		System.out.println("introduza el tipo del munimon");
+		System.out.println("Estos son Agua, Fuego, Tierra, Planta");
+		Tipo tipo2= Tipo.valueOf(muni.next()); 
+		
+		Mm2= new muniemon(nombre, vida, ataque, defensa, tipo2);
+		System.out.println("Muniemon creado con éxito.");
+		 	
 	
 			break;
          
-	case 3:	 System.out.println("programa finalizado");
+	case 3:
+		System.out.println("Datos del Muniemon:");
+        System.out.println("Nombre: " + Mm.nombre ); 
+        System.out.println("Vida: "  + Mm.vida);
+        System.out.println("Ataque: " + Mm.ataque) ;
+        System.out.println("Defensa: " + Mm.defensa); 
+        System.out.println("Tipo: " + Mm.tipo );
 		break;
-			
+		
+	case 4:
+		System.out.println("Datos del Muniemon:");
+        System.out.println("Nombre: " + Mm2.nombre ); 
+        System.out.println("Vida: "  + Mm2.vida);
+        System.out.println("Ataque: " + Mm2.ataque) ;
+        System.out.println("Defensa: " + Mm2.defensa); 
+        System.out.println("Tipo: " + Mm2.tipo );
+		
+		break;
+		
+	case 5:
+		
+		Mm.atacarMuniemon(Mm2);
+		
+		
+		break;
+		
+	case 6:
+		
+		Mm2.atacarMuniemon(Mm);
+		
+		break;
 		
         
        	
-	 
+	case 7:  System.out.println("programa finalizado");
        }
-        } while (opcion !=3); 
+       
+        } while (opcion !=7); 
         
        
         
--- Actividad18/src/pokemon/muniemon.java
+++ Actividad18/src/pokemon/muniemon.java
@@ -25,9 +25,44 @@ public class muniemon {
 		this.defensa = defensa;
 		this.tipo = tipo;
 	}
-
 	
-	}
+	public void atacarMuniemon (muniemon objetivo) {
+		
+		if (this.vida <= 0) {
+            System.out.println(this.nombre + " está muerto y no puede atacar.");
+            return;
+        }
+
+        if (objetivo.vida <= 0) {
+            System.out.println(objetivo.nombre + " está muerto y no puede ser atacado.");
+            return;
+        }
+
+        System.out.println(this.nombre + " ataca a " + objetivo.nombre + ".");
+        
+        int danoRealizado = ( this.ataque - objetivo.defensa);
+        objetivo.defensa-= danoRealizado;
+        
+        
+		
+
+        System.out.println("Se realizó un daño de " + danoRealizado + " puntos de defensa.");
+        
+      
+        
+        if (objetivo.vida <= 0) {
+            System.out.println(objetivo.nombre + " ha sido derrotado.");
+        }
+        }
+        
+        
+        
+			
+		}
+		
+	
+	
+	
 
 
 
